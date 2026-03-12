94fcdbcfa412144cc294a7c652a21ee0bffda3cf
David Warburton <david.warburton@gmail.com>
2026-03-12 11:59:38 -0400

    Change greeting from 'World' to 'to Omiur'

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@
 
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello " + (args.length > 0 ? args[0] : "World"));
+        System.out.println("Hello " + (args.length > 0 ? args[0] : "to Omiur"));
     }
 }
